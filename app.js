4686edca0a59f87f74b57e2c5ce51ffd55304e5b
Eunjae2641 <erica2641@naver.com>
2021-12-07 10:09:21 +0900

    css 수정 및 기능 구현 수정

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.post('/login', (req, res) => {
       console.log(req.body.name);
     } else {
       console.log('로그인 실패');
-      res.render('login');
+      res.send('<script>alert("로그인 실패!!(아이디 혹은 비밀번호가 잘못되었습니다.)"); location.href="/login"</script>')
     }
   });
 });
@@ -115,9 +115,7 @@ app.get('/logout', (req, res) => {
   console.log('로그아웃 성공');
   req.session.destroy(function (err) {
     //세션 파괴후 할 것들
-    res.render('index', {
-      is_logined: false,
-    });
+    res.send('<script>alert("로그아웃"); location.href="/"</script>')
   });
 });
 
@@ -172,7 +170,7 @@ app.post('/account/:id', (req, res) => {
       if (data.length == 0) {
         console.log('계좌 추가 성공');
         con.query('insert into account(id,aNUMBER, apw,NAME,bank) values(?,?,?,?,?)', [req.params.id, aNUMBER, apw, name, bank]);
-        con.query('insert into transaction(aNUMBER, sendName, MONEY, PLUS, sendNumber,sendbank,NAME) values(?,?,?,?,?,?)', [aNUMBER, "GSMbank", 10000, 0, "99999999999", "GSMbank", name]);
+        con.query('insert into transaction(aNUMBER, sendName, MONEY, PLUS, sendNumber,NAME,sendbank) values(?,?,?,?,?,?,?)', [aNUMBER, "GSMbank", 10000, 0, "99999999999",  name, "GSMbank"]);
         res.send(`<script>alert('계좌추가 성공!!'); location.href = document.referrer; </script>`); //아쉬운 점 script에 경우 location.href일 때 파라미터 값을 어떻게 전달해야하나 몰라 이 전의 페이지로 돌아가 거래내역을 눌러야함
       } else {
         console.log('계좌 추가 실패');
@@ -211,7 +209,6 @@ app.get('/delete/:aNUMBER/:id', (req, res) => {
 app.get('/remain/:aNumber/:id', (req, res) => {
   console.log('거래내역 조회');
   const body = req.body;
-  const PLUS = body.PLUS;
   con.query('select * from user where id=?', [req.params.id], (err, result) => {
     console.log(result);
     const name = result[0].NAME;
@@ -224,8 +221,8 @@ app.get('/remain/:aNumber/:id', (req, res) => {
           id: req.params.id,
           name: name,
           is_logined: body.is_logined,
-          plus: data.PLUS,
           aNUMBER: req.params.aNumber,
+          plus : data.PLUS
         });
 
       });
@@ -261,18 +258,19 @@ app.post('/withdraw/:aNumber/:id', (req, res) => {
     console.log(result);
     const name = result[0].NAME;
     if (apw == result[0].aPW) {
-      con.query('insert into transaction(aNUMBER, sendName, MONEY, PLUS, sendNumber, sendbank, NAME) values(?,?,?,?,?,?,?)', [req.params.aNumber, sendName, money, 1, sendNumber, sendbank, name], (err, data) => {
+      con.query('insert into transaction(aNUMBER, sendName, MONEY, PLUS, sendNumber, sendbank, NAME) values(?,?,?,?,?,?,?)', [req.params.aNumber, sendName, money, 1, sendNumber, sendbank, name], (err) => {
+        con.query('select * from transaction where aNumber = ?',[req.params.aNumber],(err,data)=>{
         if (err) throw err;
         console.log('이체 성공') //콤보박스로 선택하게하려 했으나 오류나서 불가했음
         req.session.save(function () {
           res.render('remain', {
             transaction: data,
-            id: req.params.id,
-            name: name,
-            is_logined: body.is_logined,
-            plus: data.PLUS,
-            aNUMBER: req.params.aNumber,
+          id: req.params.id,
+          name: name,
+          is_logined: body.is_logined,
+          aNUMBER: req.params.aNumber,
           });
+        });
         });
       });
     } else {
@@ -301,7 +299,7 @@ app.get('/mypage/:id', (req, res) => {
   });
 });
 
-app.post('/update/:id', (req, res) => { // 수정링크를 타고 들어온 데이터의 id 값과 des 값을 받아서 update ejs 파일로 넘긴다
+app.post('/update/:id', (req, res) => { 
   var id = req.params.id;
   var pw = req.body.pw;
   var name = req.body.name;
