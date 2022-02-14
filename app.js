5040c4279a0910ad2ea71931cdbdf6d30b2063ca
Eunjae2641 <erica2641@naver.com>
2022-02-14 13:37:14 +0900

    [UPDATE] MYPAGE 비밀번호 암호화

--- app.js
+++ app.js
@@ -123,30 +123,32 @@ app.get('/logout', (req, res) => {
 //list 페이지
 app.get('/list/:id', (req, res) => {
   console.log('거래내역 페이지');
-    con.query('select aNUMBER, aPW, bank, NAME from account,user where account.id=user.id and account.id = ?', [req.params.id], (err, data) => {
-      if (err) throw err;
-      var name = null;
-      if(data.length == 0){
-        con.query('select name from user where id = ?',[req.params.id],(err,result) =>{
-          name =  result[0].NAME;
-          console.log(name);
+  con.query('select aNUMBER, aPW, bank, NAME from account,user where account.id=user.id and account.id = ?', [req.params.id], (err, data) => {
+    if (err) throw err;
+    var name = null;
+    if (data.length == 0) {
+      con.query('select NAME from user where id = ?', [req.params.id], (err, result) => {
+        name = result[0].NAME;
+        console.log(name);
       });
-    }else{
+    } else {
       name = data[0].NAME;
       console.log(name);
-    }
-      req.session.save(function () {
-        res.render('list', {
-          account: data,
-          name: name,
-          id: req.params.id,
-          aNUMBER: data.aNUMBER,
-          bank: data.bank,
-          is_logined: req.params.is_logined,
-        });
+    } 
+    console.log(name); 
+    req.session.save(function () {
+      res.render('list', {
+        account: data,
+        name: name,
+        id: req.params.id,
+        aNUMBER: data.aNUMBER,
+        bank: data.bank,
+        is_logined: req.params.is_logined,
       });
     });
   });
+});
+
 //연결계좌 추가
 app.get('/account/:id', (req, res) => {
   console.log('계좌 추가 페이지');
@@ -177,7 +179,8 @@ app.post('/account/:id', (req, res) => {
       if (data.length == 0) {
         console.log('계좌 추가 성공');
         con.query('insert into account(id,aNUMBER, apw,bank) values(?,?,?,?)', [req.params.id, aNUMBER, apw, bank]);
-        con.query('insert into transaction(aNUMBER, sendName, MONEY, PLUS, sendNumber,NAME,sendbank) values(?,?,?,?,?,?,?)', [aNUMBER, "GSMbank", 10000, 0, "99999999999",  name, "GSMbank"]);
+        con.query('insert into transaction(aNUMBER, sendName, MONEY, PLUS, sendNumber,NAME,sendbank) values(?,?,?,?,?,?,?)',
+          [aNUMBER, "GSMbank", 10000, 0, "99999999999", name, "GSMbank"]);
         res.send(`<script>alert('계좌추가 성공!!'); location.href = document.referrer; </script>`); //아쉬운 점 script에 경우 location.href일 때 파라미터 값을 어떻게 전달해야하나 몰라 이 전의 페이지로 돌아가 거래내역을 눌러야함
       } else {
         console.log('계좌 추가 실패');
@@ -186,6 +189,7 @@ app.post('/account/:id', (req, res) => {
     });
   })
 });
+
 //연결 계좌 삭제
 app.get('/delete/:aNUMBER/:id', (req, res) => {
   console.log('거래내역 삭제 시도');
@@ -194,52 +198,65 @@ app.get('/delete/:aNUMBER/:id', (req, res) => {
     if (err) throw err;
     console.log(deletedata);
     console.log('거래내역 삭제 성공');
-      con.query('select aNUMBER,aPW, bank, NAME from account,user where account.id = user.id and account.id=?', [req.params.id], (err, data) => {
-        if (err) throw err;
-        if(data.length == 0){
-          con.query('select name from user where id = ?',[req.params.id],(err,result) =>{
-            const name =  result[0].NAME;
-        });
-      }else{
-        const name = data[0].name
-        req.session.save(function () {
-          res.render('list', {
-            account: data,
-            name: name,
-            id: req.params.id,
-            aNUMBER: data.aNUMBER,
-            bank: data.bank,
-            is_logined: req.params.is_logined,
-          });
-        });
-      }
+    con.query('select aNUMBER,aPW, bank, NAME from account,user where account.id = user.id and account.id=?', [req.params.id], (err, data) => {
+      if (err) throw err;
+      var name = null;
+    if (data.length == 0) {
+      con.query('select NAME from user where id = ?', [req.params.id], (err, result) => {
+        name = result[0].NAME;
+        console.log(name);
+      });
+    } else {
+      name = data[0].NAME;
+      console.log(name);
+    } 
+    console.log(name); 
+    req.session.save(function () {
+      res.render('list', {
+        account: data,
+        name: name,
+        id: req.params.id,
+        aNUMBER: data.aNUMBER,
+        bank: data.bank,
+        is_logined: req.params.is_logined,
       });
     });
+    });
   });
+});
 
+//거래내역 조회
 app.get('/remain/:aNumber/:id', (req, res) => {
   console.log('거래내역 조회');
   const body = req.body;
-  con.query('select * from user where id=?', [req.params.id], (err, result) => {
-    console.log(result);
-    const name = result[0].NAME;
-    con.query('select * from transaction where aNUMBER=?', [req.params.aNumber], (err, data) => {
-      if (err) throw err;
-      console.log('거래내역 조회 성공')
-      req.session.save(function () {
-        res.render('remain', {
-          transaction: data,
-          id: req.params.id,
-          name: name,
-          is_logined: body.is_logined,
-          aNUMBER: req.params.aNumber,
-          plus : data.PLUS
-        });
-
+  con.query('select * from finance.transaction, finance.user where transaction.NAME = user.NAME and aNUMBER=? and user.id = ?;', 
+  [req.params.aNumber,req.params.id], (err, data) => {
+    if (err) throw err;
+    console.log('거래내역 조회 성공')
+    var name = null;
+    if (data.length == 0) {
+      con.query('select NAME from user where id = ?', [req.params.id], (err, result) => {
+        name = result[0].NAME;
+        console.log(name);
+      });
+    } else {
+      name = data[0].NAME;
+      console.log(name);
+    }
+    console.log(name); 
+    req.session.save(function () {
+      res.render('remain', {
+        transaction: data,
+        id: req.params.id,
+        name: name,
+        is_logined: body.is_logined,
+        aNUMBER: req.params.aNumber,
+        plus: data.PLUS
       });
     });
   });
 });
+
 
 app.get('/withdraw/:aNumber/:id', (req, res) => {
   console.log('이체하기 작동');
@@ -269,21 +286,22 @@ app.post('/withdraw/:aNumber/:id', (req, res) => {
     console.log(result);
     const name = result[0].NAME;
     if (apw == result[0].aPW) {
-      con.query('insert into transaction(aNUMBER, sendName, MONEY, PLUS, sendNumber, sendbank, NAME) values(?,?,?,?,?,?,?)', [req.params.aNumber, sendName, money, 1, sendNumber, sendbank, name], (err) => {
-        con.query('select * from transaction where aNumber = ?',[req.params.aNumber],(err,data)=>{
-        if (err) throw err;
-        console.log('이체 성공') //콤보박스로 선택하게하려 했으나 오류나서 불가했음
-        req.session.save(function () {
-          res.render('remain', {
-            transaction: data,
-          id: req.params.id,
-          name: name,
-          is_logined: body.is_logined,
-          aNUMBER: req.params.aNumber,
+      con.query('insert into transaction(aNUMBER, sendName, MONEY, PLUS, sendNumber, sendbank, NAME) values(?,?,?,?,?,?,?)',
+        [req.params.aNumber, sendName, money, 1, sendNumber, sendbank, name], (err) => {
+          con.query('select * from transaction where aNumber = ?', [req.params.aNumber], (err, data) => {
+            if (err) throw err;
+            console.log('이체 성공') //콤보박스로 선택하게하려 했으나 오류나서 불가했음
+            req.session.save(function () {
+              res.render('remain', {
+                transaction: data,
+                id: req.params.id,
+                name: name,
+                is_logined: body.is_logined,
+                aNUMBER: req.params.aNumber,
+              });
+            });
           });
         });
-        });
-      });
     } else {
       console.log('이체 실패');
       res.send(`<script>alert("이체 실패!!(비밀번호가 틀렸습니다.)"); location.href = document.referrer; </script>`)
@@ -302,15 +320,13 @@ app.get('/mypage/:id', (req, res) => {
         user: data,
         name: data[0].NAME,
         id: req.params.id,
-        pw: data[0].PW,
-        email: data[0].EMAIL,
         is_logined: req.params.is_logined,
       });
     });
   });
 });
 
-app.post('/update/:id', (req, res) => { 
+app.post('/update/:id', (req, res) => {
   var id = req.params.id;
   var pw = req.body.pw;
   var name = req.body.name;
